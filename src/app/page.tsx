6f9fa95634020a4f59373a330701029e858ceaec
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-06-14 14:11:26 +0300

    chore(deps): remove react-icons and install instead unocss compatible icon presets

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,9 @@
-import Stack from "@/components/layout/Stack/Stack";
-import Group from "@/components/layout/Group/Group";
-import Center from "@/components/layout/Center/Center";
-
 export default function Home() {
     return (
         <div className="text-dark-1">
-            <Stack>
-                <div>314</div>
-                <div>314</div>
-                <div>314</div>
-            </Stack>
-            <Group className={""}>
-                <div>234234</div>
-                <div>234234</div>
-                <div>234234</div>
-                <div>234234</div>
-            </Group>
             <h2 text="2xl">
                 234
             </h2>
-            <Center className="h-svh">
-                qwer
-            </Center>
             1234
             <button className="b-rose b-1 b-solid b-b-amber bg-gray text-white">1234</button>
         </div>
