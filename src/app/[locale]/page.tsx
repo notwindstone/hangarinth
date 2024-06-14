1801218fbfd9a8b90aff00a2bd6c3576bbc98ab3
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-06-14 20:56:40 +0300

    feat: implement i18n

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,8 +1,12 @@
 import Hero from "@/components/Hero/Hero";
+import {useTranslations} from "next-intl";
 
 export default function Home() {
+    const t = useTranslations('Index');
+
     return (
         <div className="flex items-center justify-center w-full">
+            <p>{t('title')}</p>
             <Hero />
         </div>
     );
