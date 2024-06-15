aeb45d26b9b0c81c1798286d95d1a2c837399481
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-06-15 10:01:56 +0300

    feat(styling): change Hero component styles

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -5,21 +5,21 @@ export default function Hero() {
         <div className="flex justify-between ml-16 mr-16 mb-16 mt-36 w-288 h-83">
             <div className="flex flex-col gap-8">
                 <div className="flex flex-col gap-4">
-                    <p className="font-extrabold text-18 text-soft-blue line-height-none">
+                    <p className="font-bold text-14 text-soft-blue line-height-none">
                         Hangarinth
                     </p>
-                    <p className="font-extrabold text-18 text-dark-1 line-height-none">
+                    <p className="font-bold text-14 text-dark-1 line-height-none">
                         Find your favourite plugins or mods
                     </p>
                 </div>
                 <div className="flex gap-8">
                     <button
-                        className="text-6 font-semibold text-white bg-soft-blue rounded-full w-fit pl-12 pr-12 pt-4 pb-4"
+                        className="text-3.5 font-semibold text-white bg-soft-blue rounded-full w-fit pl-6 pr-6 pt-2 pb-2"
                     >
                         Search
                     </button>
                     <button
-                        className="text-6 font-semibold text-dark-1 bg-gray-3 rounded-full w-fit pl-12 pr-12 pt-4 pb-4"
+                        className="text-3.5 font-semibold text-dark-1 bg-gray-3 rounded-full w-fit pl-6 pr-6 pt-2 pb-2"
                     >
                         About
                     </button>
