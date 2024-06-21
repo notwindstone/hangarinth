48d47389b14a49f4001358f463eb473eff5f43d3
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-06-21 15:01:13 +0300

    feat: remove all testing code

--- src/app.vue
+++ src/app.vue
@@ -4,19 +4,13 @@
     <NuxtRouteAnnouncer />
     <NuxtLink to="/about">About</NuxtLink>
     <NuxtLink to="/">Home</NuxtLink>
-    <CustomComponent>
-      This is really fast
-    </CustomComponent>
-    <button>
-      123
-    </button>
   </div>
 </template>
 
 <style>
 @import '../node_modules/@unocss/reset/tailwind.css';
 @import '../node_modules/@unocss/reset/normalize.css';
-div {
+body {
   font-family: Inter, sans-serif;
 }
 </style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,8 +1,5 @@
 <template>
   <div>
     <h1>Welcome to the homepage</h1>
-    <CustomComponent>
-      This is an auto-imported component
-    </CustomComponent>
   </div>
 </template>
