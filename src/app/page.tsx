939c4ebe0a912479028a46a57a061dd85eea1105
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-06-21 07:38:18 +0300

    feat: transform project from Next to Nuxt

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Hero from "@/components/Hero/Hero";
-
-export default function Home() {
-    return (
-        <div className="flex items-center justify-center w-full">
-            <Hero />
-        </div>
-    );
-}
